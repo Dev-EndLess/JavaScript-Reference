aa75c8e3864332dcacc5b8a5dd2a64940883c2e9
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-20 16:08:22 +0100

    added if else examples

---
+++ BasicJS/Conditional Statement & Comparision Operators/app2.js
@@ -0,0 +1,34 @@
+//* * * If... Else * * *//
+
+let pizza = true
+
+if (pizza) {
+  console.log('Pizza is Great, I love Pizza')
+} else {
+  console.log('Pizza is so Bad, I hate Pizza')
+}
+
+// * * * Comparision Operators * * *// 
+// > maggiore
+// >= maggiore o uguale
+// < minore
+// <= minore o uguale
+// == uguale
+// != non uguale
+
+let age = 5
+
+if (age < 3) {
+  console.log('You are a Baby')
+} else if ( age < 17) {
+  console.log('You are a Child')
+} else if ( age >= 18) {
+  console.log('You are an Adult')
+}
+
+if (age == 18) {
+  console.log('You have 18 year old')
+}
+if (age != 18) {
+  console.log('You DONT have 18 year old')
+}
