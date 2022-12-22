f9a2cca83f08cf859ebdbd5807a3a6b94cdac406
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-22 17:24:09 +0100

    added modules files to fix

---
+++ 5 - ES6/p4 - Modules ( Export - Import )/app2.js
@@ -0,0 +1,17 @@
+//* * * IMPORT * * *//
+
+import sum from './app.js'
+sum(5, 5)
+
+import { somma, sottrazione, moltiplicazione } from './app.js'
+
+
+
+// import * as . . . . from './funzioni.js'
+// importa tutto dal file
+
+
+somma(2, 2)
+sottrazione(3, 3)
+moltiplicazione(4, 4)
+
