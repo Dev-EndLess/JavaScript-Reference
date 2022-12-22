8a9ea8a1a3039c52b4487dc21b4376aa5e5838ca
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-22 17:24:09 +0100

    added modules files to fix

---
+++ 5 - ES6/p4 - Modules ( Export - Import )/app.js
@@ -0,0 +1,28 @@
+//* * * EXPORT * * *//
+
+export default function sum(n1, n2) {
+  return n1 + n2
+}
+
+
+
+
+// const somma = (a, b) => {
+//   return a + b
+// }
+
+
+// const sottrazione = (x, y) => {
+//   return x - y;
+// }
+
+// const moltiplicazione = (num1, num2) => {
+//   return num1 * num2
+// }
+
+// export { somma, sottrazione, moltiplicazione }
+
+
+
+//-----------------------------------------------------------
+
