27740f02840f438d3e6543456a96302fc4dacb28
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-23 10:47:10 +0100

    added data

---
+++ 4 - High Order Functions/posts.js
@@ -0,0 +1,38 @@
+export const posts = [
+  {
+    userId: 1,
+    id: 1,
+    title: "Lorem",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, doloremque."
+  },
+  {
+    userId: 2,
+    id: 2,
+    title: "ipsum",
+    body: "Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  },
+  {
+    userId: 3,
+    id: 3,
+    title: "consectetur",
+    body: "Lorem sit amet consectetur adipisicing elit. Nostrum, doloremque."
+  },
+  {
+    userId: 4,
+    id: 4,
+    title: "adipisicing",
+    body: "Lorem ipsum dolor  adipisicing elit. Nostrum, doloremque."
+  },
+  {
+    userId: 5,
+    id: 5,
+    title: "elit",
+    body: "Lorem ipsum dolor sit amet Nostrum, doloremque."
+  },
+  {
+    userId: 6,
+    id: 6,
+    title: "dolor",
+    body: "sit amet consectetur adipisicing elit. Nostrum, doloremque."
+  },
+]
