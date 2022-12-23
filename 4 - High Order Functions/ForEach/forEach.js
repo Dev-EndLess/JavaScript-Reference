8ef5917e46cecac400f8e0d26cba765a2181363a
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-23 11:50:47 +0100

    added forEach example

---
+++ 4 - High Order Functions/ForEach/forEach.js
@@ -0,0 +1,13 @@
+const array = [
+  { name: 'Sara Connor', age: 42, gender: 'Female', points: 7, email: 'SaraConnor@gmail.com' },
+  { name: 'John Connor', age: 14, gender: 'Male', points: 4, email: 'JohnConnor@gmail.com' },
+  { name: 'Ada Wong', age: 32, gender: 'Female', points: 11, email: 'AdaWong@gmail.com' },
+  { name: 'Albert Wesker', age: 37, gender: 'Male', points: 22, email: 'AlbertWesker@gmail.com' },
+  { name: 'Jill Valentine', age: 24, gender: 'Female', points: 15, email: 'JillValentine@gmail.com' },
+  { name: 'Leon Kennedy', age: 27, gender: 'Male', points: 18, email: 'LeonKennedy@hotmail.com' },
+]
+
+
+array.forEach(num => console.log(num.points * 2));
+array.forEach(num => console.log('I Personaggi Selezionabili sono ' + num.name ));
+array.forEach(num => console.log('Puoi contattarli all indirizzo ' + num.email ));
