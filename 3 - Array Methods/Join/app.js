e825ac7bc515eb42cd6373cff896fc76d3ebbbeb
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-27 10:07:48 +0100

    added JOIN examples

---
+++ 3 - Array Methods/Join/app.js
@@ -0,0 +1,16 @@
+//* * * JOIN * * *//
+
+// Definition and Usage
+// The join() method returns an array as a string.
+// The join() method does not change the original array.
+// Any separator can be specified. The default is comma (,).
+
+const nation = ['Italy', 'France', 'Spain', 'Germany']
+
+// const result = nation.join(' - ')
+// const result = nation.join(', ')
+// const result = nation.join('')
+const result = nation.join(' / ')
+
+console.log(result)
+console.log(`I want Visit ${result}` )
