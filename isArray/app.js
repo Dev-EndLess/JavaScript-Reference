e99d3d0bab46aa087ea255d9a1afc061768e7a12
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-28 09:59:12 +0100

    isArray Added

--- isArray/app.js
+++ isArray/app.js
@@ -1,2 +1,14 @@
-//* * *  * * *//
+//* * * isArray * * *//
 
+// Definition and Usage
+// The isArray() method returns true if an object is an array, otherwise false.
+
+const names = ['endless', 'lyna', 'ivan']
+const string = 'Hello Dude'
+const number = 22
+const number2 = [22]
+
+console.log(Array.isArray(names))
+console.log(Array.isArray(string))
+console.log(Array.isArray(number))
+console.log(Array.isArray(number2))
