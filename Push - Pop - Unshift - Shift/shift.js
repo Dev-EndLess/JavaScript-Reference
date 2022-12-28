3487b852be57b26a9405c3e4f623a8bcdde68038
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-28 09:32:00 +0100

    added SHIFT - UNSHIFT examples

---
+++ Push - Pop - Unshift - Shift/shift.js
@@ -0,0 +1,14 @@
+//* * * SHIFT * * *//
+console.log('* * * unshift * * *')
+
+// Definition and Usage
+// The shift() method removes the first item of an array.
+// The shift() method changes the original array.
+// The shift() method returns the shifted element.
+
+const numbers5 = [1, 2, 3, 4, 5, 6]
+
+const firstnumber = numbers5.shift()
+
+console.log(firstnumber)
+console.log(numbers5)
---
+++ Push - Pop - Unshift - Shift/unshift.js
@@ -0,0 +1,19 @@
+//* * * UNSHIFT * * *//
+console.log('* * * unshift * * *')
+
+// Definition and Usage
+// The unshift() method adds new elements to the beginning of an array.
+// The unshift() method overwrites the original array.
+
+const numbers4 = [3, 4, 5]
+
+// numbers4.unshift(1, 2)
+numbers4.unshift(-1, 0, 1, 2)
+
+console.log(numbers4)
+
+
+
+
+
+
