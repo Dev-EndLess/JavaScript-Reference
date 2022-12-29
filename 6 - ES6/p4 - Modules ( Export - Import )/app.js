0a1b77c5a5e21b72ce2073ade41ecb50722356a9
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-29 12:01:13 +0100

    fixing folders

--- 6 - ES6/p4 - Modules ( Export - Import )/app.js
+++
@@ -1,9 +0,0 @@
-//* * * IMPORT * * *//
-
-// import in curly brackets if is not default export
-import User, { printName, printAge } from './app2.js'
-
-const user = new User('Bob', 11)
-printName(user)
-printAge(user)
-console.log(user)
