b6592583bb531888bfc54ef1826b4f712f9d7f6f
Dev-EndLess <endlesspain1988@gmail.com>
2022-12-29 12:01:01 +0100

    Fetch Async Await

---
+++ 6 - Asynchronous JS Ajax-Fetch API/6. Fetch (Async-Await)/HTTP Library (Fetch with Async - Await)/app.js
@@ -0,0 +1,50 @@
+// Users Data
+ 
+const person1 = {
+  name: "Leon Kennedy",
+  username: "Leon",
+  email: "Leon-Kennedy@gmail.com",
+}
+
+const person2 = {
+  name: "Jill Valentine",
+  username: "Jill",
+  email: "Jill-Valentine@gmail.com",
+}
+
+
+const http = new EasyHTTP();
+
+
+// Get Users
+
+const users = http
+  .get("https://jsonplaceholder.typicode.com/users")
+  .then((data) => console.log(data))
+  .catch((err) => console.log(err));
+
+
+
+// Create Post
+
+http.post('https://jsonplaceholder.typicode.com/users', person1)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+
+
+// Update Post
+
+http.put('https://jsonplaceholder.typicode.com/users/2', person2)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+
+
+// Delete Post
+
+http.delete('https://jsonplaceholder.typicode.com/users/2', person2)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+
